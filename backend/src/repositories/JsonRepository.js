59a690e576b8c3906c8ec59d8b5643e4b0e3a69d
Mateus Belchior Almeida <mateus.belchior.98@gmail.com>
2026-08-04 10:53:39 -0300

    .

--- backend/src/repositories/JsonRepository.js
+++ backend/src/repositories/JsonRepository.js
@@ -15,7 +15,7 @@ export default class JsonRepository {
 
         try {
 
-            const data = await fs.readFile(this.file, "utf8");
+            const data = await fs.readFileSync(this.file, "utf8");
 
             return JSON.parse(data);
 
@@ -29,7 +29,7 @@ export default class JsonRepository {
 
     async write(data) {
 
-        await fs.writeFile(
+        await fs.writeFileSync(
             this.file,
             JSON.stringify(data, null, 4)
         );
