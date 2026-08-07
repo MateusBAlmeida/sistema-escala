28a19abf18fe7577c35902bb8c821ccfdb3e7e30
Mateus Belchior Almeida <mateus.belchior.98@gmail.com>
2026-08-07 09:15:40 -0300

    .

--- backend/src/services/EscalaService.js
+++ backend/src/services/EscalaService.js
@@ -298,7 +298,7 @@ class EscalaService {
                         funcionario.totalEscalas,
 
                     ultimaEscala:
-                        funcionario.ultimaEscala.getUTCDate() === 1
+                        funcionario.ultimaEscala.toLocaleDateString("pt-BR", { timeZone: "UTC" })
                 }
             );
         }
